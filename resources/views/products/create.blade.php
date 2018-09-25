169e3a30fb414c02d60554432ff6bbd29371fa7b
Irene <ireneprins11@hotmail.com>
2018-09-25 21:28:17 +0200

    error form

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,5 +3,14 @@
 
 @section('content')
     <h3>Add Your Product</h3>
-  
+    
+    {!! Form::open(['action' -> 'ProductsController@store', 'method' -> 'POST') !!}
+    //Form::open(['action' => ['Controller@method', $user->id]])
+    <div class="form-group">
+        {{Form::label('title', 'Title')}}
+        {{Form::text('title', ' ', ['class' -> 'form-control', 'placeholder' -> 'Title'])}}
+
+
+    </div>
+    {!! Form::close() !!}
 @endsection
