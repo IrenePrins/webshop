a5a1c5526836ac75eb7ccb7220d0b6a678d16398
Irene <ireneprins11@hotmail.com>
2018-10-14 21:10:59 +0200

    admin werkt

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,11 @@ Route::get('/users/{id}', function($id, $name){
 Route::get('/', 'ProductsController@index');
 Route::resource('products', 'ProductsController');
 
-Route::prefix('admin')->group(function(){
-    Route::get('login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-    Route::post('login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login.submit');
-    Route::get('/', 'AdminController@index')->name('admin.dashboard');
-});
+
+Route::get('admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+Route::post('admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
+
 
 
 Auth::routes();
