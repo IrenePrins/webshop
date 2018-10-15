de19832826f00761ba259d540ce31537fb07ea91
Irene <ireneprins11@hotmail.com>
2018-10-15 15:15:26 +0200

    auth bugs fix

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -19,6 +19,11 @@
                             <a class="nav-link" href="/products/create/">Create Product</a>
                             </li>
                         @endif
+
+                        <form class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
                      
                 </ul>
 
