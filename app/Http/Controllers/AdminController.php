a9fc74916105948e971421b8957588449914b1c5
Irene <ireneprins11@hotmail.com>
2018-10-16 11:04:25 +0200

    begin eager loading on admindashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Admin;
 use App\Product;
+use App\User;
 
 class AdminController extends Controller
 {
@@ -25,7 +26,9 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin.adminDashboard');
+        $users = DB::select('SELECT products.title, users.name FROM products INNER JOIN users ON products.user_id = users.id');
+
+        return view('admin.adminDashboard', ['users' => $users]);
     }
 }
 ?>
