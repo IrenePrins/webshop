be1f4433a15bc5ad7e7ea4604b8ef697afb0cfa6
Irene <ireneprins11@hotmail.com>
2018-10-16 23:14:56 +0200

    goede query nog niet goed in view idk hoe dat moet nog

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,7 +26,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        $users = DB::select('SELECT products.title, users.name FROM products INNER JOIN users ON products.user_id = users.id');
+        $users = DB::select('SELECT users.name, COUNT(products.user_id) FROM products INNER JOIN users ON users.id = products.user_id GROUP BY products.user_id');
 
         return view('admin.adminDashboard', ['users' => $users]);
     }
--- resources/views/admin/adminDashboard.blade.php
+++ resources/views/admin/adminDashboard.blade.php
@@ -37,7 +37,7 @@
                     @foreach($users as $user)
                     <tr>
                         <td>{{$user->name}}</td>
-                        <td>{{$user->title}}</td>
+                        <td>{{$user->products.user_id}}</td>
                     </tr>
                     @endforeach
                 </table>
