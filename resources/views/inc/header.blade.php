4b3938a5c4c7c16df05f0b5b793f45393d59ad5c
Irene <ireneprins11@hotmail.com>
2018-10-29 13:41:55 +0100

    migration status checken of in master

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -16,4 +16,7 @@
     
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+        <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
     </head>
